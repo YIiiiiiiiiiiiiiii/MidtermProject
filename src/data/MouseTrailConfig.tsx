f64de6f53484cc91027e55e47a44887a53d1a3f1
powen <powen0966091716@gmail.com>
2024-11-16 10:17:01 +0800

    Update MouseTrailConfig.tsx

--- src/data/MouseTrailConfig.tsx
+++ src/data/MouseTrailConfig.tsx
@@ -1,8 +1,8 @@
 export const MouseTrailConfig = {
     color: "rgba(95, 190, 255, 0.8)",
     idleAnimation: true,
-    idleAnimationCount: 10,
+    idleAnimationCount: 20,
     inverted: false,
     size: 20,
-    trailCount: 15,
+    trailCount: 20,
   };
